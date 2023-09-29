228e0993f8e4882c5f74cec8cf90d4be2b861470
AlexSiuuu <alextron45@gmail.com>
2023-09-29 15:45:47 -0500

    subiendo las rutas

--- proyecto-tareas/src/main.jsx
+++ proyecto-tareas/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './routes/Login.jsx';
+import Registro from './routes/Registro.jsx';
+import Dashboard from './routes/Dashboard.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/Registro",
+    element: <Registro />,
+  },
+  {
+    path: "/Dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
