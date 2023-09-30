77ac105b446a8de202adc6dbfb2577299156117a
AlexSiuuu <alextron45@gmail.com>
2023-09-30 13:45:51 -0500

    agregando estilos

--- proyecto-tareas/src/routes/Login.jsx
+++ proyecto-tareas/src/routes/Login.jsx
@@ -1,3 +1,40 @@
+import '../index.css'
+
 export default function Login() {
-    return <h1>Login</h1>
+    return (
+
+        <div>
+
+            <div className="img">
+
+                <div className="contenedor">
+
+                    <div className="formulario">
+
+                        <form>
+                        
+                            <h1 className='titulo-login'>Entrar</h1><br /><br />
+                                            
+                            <label className='nombre-usuario'>Nombre de Usuario:</label><br />
+                            <input className='campo-usuario' type="text" /><br /><br />
+                                            
+                            <label className='nombre-usuario'>Ingrese Contraseña:</label><br />
+                            <input className='campo-usuario' type="password" /><br /><br />
+
+                            <center><button className='boton-login'>Ingresar</button></center>
+
+                        </form>
+                    </div>
+
+                </div>
+
+                
+
+            </div>
+
+        </div>
+        
+        
+    );
+     
 }
